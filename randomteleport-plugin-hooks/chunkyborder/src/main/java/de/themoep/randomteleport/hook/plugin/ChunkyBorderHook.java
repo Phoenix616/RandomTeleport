548d873790d648fb09eee01cddd05f8a11f4fbc6
Phoenix616 <max@themoep.de>
2023-01-16 17:06:16 +0100

    Update to latest releases of Chunky(Border) (fixes #55)

--- randomteleport-plugin-hooks/chunkyborder/src/main/java/de/themoep/randomteleport/hook/plugin/ChunkyBorderHook.java
+++ randomteleport-plugin-hooks/chunkyborder/src/main/java/de/themoep/randomteleport/hook/plugin/ChunkyBorderHook.java
@@ -9,10 +9,12 @@ import org.popcraft.chunkyborder.BorderData;
 import org.popcraft.chunkyborder.ChunkyBorder;
 
 public class ChunkyBorderHook implements WorldborderHook {
-    private final ChunkyBorder plugin;
+    private final Plugin plugin;
+    private final ChunkyBorder chunkyBorder;
 
     public ChunkyBorderHook() {
-        this.plugin = (ChunkyBorder) Bukkit.getPluginManager().getPlugin("ChunkyBorder");
+        this.plugin = Bukkit.getPluginManager().getPlugin("ChunkyBorder");
+        chunkyBorder = plugin.getServer().getServicesManager().getRegistration(ChunkyBorder.class).getProvider();
     }
 
     @Override
@@ -22,19 +24,19 @@ public class ChunkyBorderHook implements WorldborderHook {
 
     @Override
     public Location getCenter(World world) {
-        BorderData borderData = plugin.getBorders().get(world.getName());
+        BorderData borderData = chunkyBorder.getBorders().get(world.getName());
         return new Location(world, borderData.getCenterX(),0D,borderData.getCenterZ());
     }
 
     @Override
     public double getBorderRadius(World world) {
-        BorderData borderData = plugin.getBorders().get(world.getName());
+        BorderData borderData = chunkyBorder.getBorders().get(world.getName());
         return borderData.getRadiusX();
     }
 
     @Override
     public boolean isInsideBorder(Location location) {
-        BorderData borderData = plugin.getBorders().get(location.getWorld().getName());
+        BorderData borderData = chunkyBorder.getBorders().get(location.getWorld().getName());
         return borderData.getBorder().isBounding(location.getBlockX(),location.getBlockZ());
     }
 
